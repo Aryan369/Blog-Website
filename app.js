1393e53b2c850f64baa6d97f00f0ba06ad1df08c
Aryan-369 <71432096+Aryan369@users.noreply.github.com>
2021-08-30 16:47:47 +0530

    Added Database

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
-const year = require(`${__dirname}/date.js`);
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,47 +16,64 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
-app.get("/", (req, res) => {
-  res.render("home", { Year: year.getYear(), homeContent: homeStartingContent, newPosts: posts});
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", { Year: year.getYear(), about: aboutContent});
+app.get("/compose", function(req, res){
+  res.render("compose");
 });
 
-app.get("/contact", (req, res) => {
-  res.render("contact", { Year: year.getYear(), contactDetails: contactContent});
-});
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
 
-app.get("/compose", (req, res) => {
-  res.render("compose", { Year: year.getYear()});
-});
 
-app.get("/posts/:postName", (req, res) => {
-  const requestedID = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(post) {
-    const postID = _.lowerCase(post.title);
-
-    if(postID === requestedID){
-      console.log("Match found!");
-
-      res.render("post", { Title: postID, content: post.content, Year: year.getYear()})
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
     }
   });
 });
 
+app.get("/posts/:postId", function(req, res){
 
+const requestedPostId = req.params.postId;
 
-app.post("/compose", (req, res) => {
-  let post = {title: req.body.postTitle, content: req.body.postContent}
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
 
-  posts.push(post);
-
-  res.redirect("/");
 });
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
